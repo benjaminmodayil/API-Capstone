36df72cd1e473ce50b10405537098f3f7d56ab11
Benjamin Modayil <modayilme@gmail.com>
2018-02-01 12:13:41 -0500

    Update 2018-02-01T17:13:39.333Z

---
+++ js/index.js
@@ -0,0 +1,239 @@
+let newsAPIKey = 'ed14c7ddee15497fb440c9369baf1371'
+
+// double check form
+// button position when form is expanded (also double check on first input width)
+// menu highlight when on saved articles
+// saved page formatting
+
+// remove, functionality when click on saved button
+// 👆 same for saved page
+// issue of search going when nothing in input
+
+const el = document.querySelector('data-index')
+class ArticlePage {
+  constructor(el) {
+    this.el = el
+    this.setupDOM()
+    // this.bindEvents()
+  }
+
+  setupDOM() {
+    this.categories = [
+      'business',
+      'entertainment',
+      'general',
+      'health',
+      'science',
+      'sports',
+      'technology'
+    ]
+
+    this.indexPage = document.querySelector('[data-index]')
+    this.container = this.indexPage.querySelector('.container')
+
+    $(this.container).append(`<div class="js-button-container"></div>
+    <h2 class="h2 js-headline-and-search-results"><span class="js-headline-and-search-results__pre-text">Headlines for</span> <span class="js-result-title">${setDate()}</span></h2>
+    <ul></ul>`)
+
+    this.categories.forEach(item =>
+      $('.js-button-container').append(`
+    <button class="js-category" data-category="${item}">${item}</button>
+    `)
+    )
+
+    this.data = []
+    this.currentArticles = []
+
+    $('.home-page').on('click', '[data-category]', this.categoryFetch.bind(this))
+    $('form').on('submit', this.handleForm.bind(this))
+  }
+
+  fetchQuery(query) {
+    let myHeaders = new Headers()
+    let myInit = { method: 'GET', headers: myHeaders, mode: 'cors', cache: 'default' }
+
+    let currentDay = new Date()
+    currentDay = currentDay.toISOString().substring(0, 10)
+
+    fetch(
+      query
+        ? query
+        : `https://newsapi.org/v2/everything?q=from=${currentDay}&language=en&sortBy=popularity&apiKey=${newsAPIKey}`,
+      myInit
+    )
+      .then(function(response) {
+        return response.json()
+      })
+      .then(response => {
+        this.data = response.articles
+        this.data = unique(this.data, e => e.title)
+        this.currentArticles = this.data
+        return this.data
+      })
+      .then(data => {
+        this.renderArticles(data)
+      })
+  }
+
+  categoryFetch(e) {
+    let category = e.currentTarget.dataset.category
+    $('.js-headline-and-search-results__pre-text').text('News related to')
+    $('.js-result-title').text(category)
+    category = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${newsAPIKey}`
+    articlePage.fetchQuery(category)
+    document
+      .querySelector('.js-headline-and-search-results')
+      .scrollIntoView({ behavior: 'smooth' })
+  }
+
+  renderArticles(data) {
+    $('ul').html('')
+    data.map((item, i) => {
+      let type, element
+
+      type = i % 5 === 0 ? 'span-half' : 'span-quarter'
+      element = createCard(item, type)
+      element.style.animationDelay = `${i * 5 / 50}s`
+
+      listContainer.appendChild(element)
+    })
+
+    $('.js-card__button').on('click', function(e) {
+      let $current, title, description, author, url, urlToImage
+
+      $current = $(e.currentTarget)
+      title = $current
+        .closest('li')
+        .find('.js-card__text')
+        .find('h2')
+        .text()
+
+      description = $current
+        .closest('li')
+        .find('.js-card__text')
+        .find('.js-card__description')
+        .text()
+
+      author = $current
+        .closest('li')
+        .find('.js-card__text')
+        .find('.js-card__author')
+        .text()
+
+      url = $current.closest('li').find('a')[0].href
+
+      urlToImage = $current
+        .closest('li')
+        .find('a')
+        .first('a')
+        .find('.js-card__img')[0]
+        .style.backgroundImage.slice(4, -1)
+        .replace(/"/g, '')
+
+      if ($(this).hasClass('js-add-to-saved')) {
+        let iconDelete = document.createElement('img')
+        let iconSave = document.createElement('img')
+
+        $(this).attr('title', 'saved')
+
+        $(iconDelete)
+          .attr('src', './images/icon-delete.svg')
+          .addClass('js-icon-delete')
+
+        $(iconSave)
+          .attr('src', './images/icon-save.svg')
+          .addClass('js-icon-save')
+        $(this).append(iconDelete)
+
+        $(this).append(iconSave)
+
+        $(this).addClass('js-remove-saved')
+
+        $(this).removeClass('js-add-to-saved')
+
+        editLocalStorage({ title, description, author, url, urlToImage })
+      } else if ($(this).hasClass('js-remove-saved')) {
+        removeLocalStorage(title)
+        $('.js-icon-delete').remove()
+        $('.js-icon-save').remove()
+        $(this).removeClass('js-remove-saved')
+        $(this).addClass('js-add-to-saved')
+      }
+    })
+  }
+
+  handleForm(e) {
+    e.preventDefault()
+    let $queryValue, $date1Value, $date2Value, $selectValue, $URL
+
+    $queryValue = $('input[name="query"]').val() || ''
+    $date1Value = $('input[name="date-1"').val()
+      ? `&from=${$('input[name="date-1"').val()}`
+      : ''
+    $date2Value = $('input[name="date-2"') ? `&to=${$('input[name="date-2"').val()}` : ''
+    $selectValue = `&category=${$('select').val()}` || ''
+    $URL = `
+  https://newsapi.org/v2/everything?q=${$queryValue}${$date1Value}&sortBy=popularity&apiKey=${newsAPIKey}
+  `
+    if ($queryValue.trim() === '') {
+      return
+    } else {
+      $('.js-headline-and-search-results__pre-text').text('News related to')
+      $('.js-result-title').text($queryValue)
+
+      this.fetchQuery($URL)
+      document
+        .querySelector('.js-headline-and-search-results')
+        .scrollIntoView({ behavior: 'smooth' })
+
+      $('input').val('')
+    }
+  }
+}
+
+let articlePage = new ArticlePage()
+
+let listContainer = document.querySelector('ul')
+listContainer.classList.add('js-news-list')
+
+function formFocus() {
+  $('input:text:visible:first').focus()
+}
+
+$('.home-header__more').on('click', showFields)
+
+function showFields() {
+  let $form = $('.hidden-fields')
+  let $searchButton = $('.search-button')
+
+  if ($form.attr('data-isopen') === 'false') {
+    $('.home-header__more__text').text('less')
+    $form.toggleClass('screenreader-only--with-space')
+    $form.attr('data-isopen', true)
+    setTimeout(() => {
+      $form.toggleClass('js-initialize-form')
+      $form.addClass('js-transition')
+    }, 50)
+    $searchButton.removeClass('search--width')
+    $searchButton.find('.screenreader-only').removeClass('screenreader-only')
+    $('input#query').removeClass('input--width')
+    $searchButton.find('img').addClass('js-img--size')
+    $form.append($searchButton)
+  } else if ($form.attr('data-isopen') === 'true') {
+    $('.home-header__more__text').text('more')
+    $form.removeClass('js-transition')
+    $form.toggleClass('screenreader-only--with-space')
+    $form.attr('data-isopen', false)
+    setTimeout(() => {
+      $form.toggleClass('js-initialize-form')
+    }, 50)
+
+    $searchButton.addClass('search--width')
+    $searchButton.find('span').addClass('screenreader-only')
+    $searchButton.find('img').removeClass('js-img--size')
+    $('input#query').addClass('input--width')
+    $('input#query').after($searchButton)
+  }
+}
+
+$(setDate(), formFocus(), articlePage.fetchQuery())
---
+++ js/shared.js
@@ -0,0 +1,90 @@
+function editLocalStorage(newItem) {
+  var oldItems = JSON.parse(localStorage.getItem('itemsArray')) || []
+  let tempItems = oldItems
+  oldItems = []
+
+  oldItems.push(newItem)
+  let allTheNew = oldItems.concat(tempItems)
+  console.log(allTheNew)
+
+  localStorage.setItem('itemsArray', JSON.stringify(allTheNew))
+}
+
+function removeLocalStorage(title) {
+  var oldItems = JSON.parse(localStorage.getItem('itemsArray')) || []
+  let newItems = oldItems.filter(item => item.title !== title)
+
+  localStorage.setItem('itemsArray', JSON.stringify(newItems))
+}
+
+function setDate() {
+  var date = new Date()
+  var weekday = new Array(7)
+  weekday[0] = 'Sunday'
+  weekday[1] = 'Monday'
+  weekday[2] = 'Tuesday'
+  weekday[3] = 'Wednesday'
+  weekday[4] = 'Thursday'
+  weekday[5] = 'Friday'
+  weekday[6] = 'Saturday'
+  let currentDay = weekday[date.getDay()]
+  return currentDay
+}
+
+function unique(arr, f) {
+  const vArr = arr.map(f)
+  return arr.filter((_, i) => vArr.indexOf(vArr[i]) === i)
+}
+
+function createCard(item, type, iconIsRemove = false) {
+  let element = document.createElement(`li`)
+  element.classList.add('js-card')
+  element.classList.add('js-fadeInDown')
+  element.classList.add(`js-${type}`)
+
+  let descriptionCheck =
+    type === 'span-half'
+      ? `
+        <p class="js-card__description">${item.description.substr(0, 85)}...</p>`
+      : ''
+
+  let imageCheck =
+    item.urlToImage === undefined ||
+    item.urlToImage === null ||
+    item.urlToImage.charAt(0) === `/`
+      ? true
+      : false
+
+  let title = item.title === null ? (element.innerHTML = '') : item.title
+  let authorTemplate = `<p class="js-card__author">${item.author}</p>`
+
+  let authorCheck = item.author === null ? true : false
+
+  let imageTemplate = `<a class="js-card__img-container" href=${item.url}>
+      <div class="js-card__img" style="background-image:url(${
+        item.urlToImage
+      })" role="img" alt="">
+      </div>
+    </a>
+  `
+  let correctIcon = iconIsRemove
+    ? `<img class="icon-file-delete" src="./images/icon-delete.svg">`
+    : `<img class="icon-file-add" src="./images/file-add.svg">`
+
+  let correctClass = iconIsRemove ? `js-remove-saved` : `js-add-to-saved`
+
+  element.innerHTML = `
+  ${imageCheck === false ? imageTemplate : ''}
+       <div class="js-card__container ${imageCheck === true ? 'js-stretch' : ''}">
+        <div class="js-card__text">
+          <a href=${item.url}>
+            <h2>${item.title}</h2>
+            ${authorCheck ? '' : authorTemplate}
+            ${descriptionCheck}
+          </a>
+        </div>
+        <button class="js-card__button ${correctClass}" type="button" title="save article"><span class="screenreader-only">save article</span>${correctIcon}</button>
+       </div>
+      `
+  return element
+}
